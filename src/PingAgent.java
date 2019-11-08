ae78a711ed10a90f6a5a50ba512d3e12218ec914
laura <laura.carrera.e@estudiant.upc.edu>
2019-11-08 15:27:28 +0100

    1st commit

--- src/PingAgent.java
+++ src/PingAgent.java
@@ -50,6 +50,7 @@ public class PingAgent extends Agent {
 		}
 
 		public void action() {
+			System.out.println("hola");
 			ACLMessage  msg = myAgent.receive();
 			if(msg != null){
 				ACLMessage reply = msg.createReply();
